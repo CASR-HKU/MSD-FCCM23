361ed4b14598a9638bce3f275744af64196cfba6
lloo099 <zhoutomas177@gmail.com>
2023-03-27 11:54:04 +0800

    msd_analysis_cleanup

--- software/msd_quant/hamha_analysis/adaptive_search_latency_vgg16.py
+++ software/msd_quant/hamha_analysis/adaptive_search_latency_vgg16.py
@@ -85,42 +85,42 @@ with open('/home/jjc/hamha_quant/hamha_quant/hamha_analysis/csv/ultra96_vgg16.cs
     # speed_up = total_latency8_8_8/expect_latency
 
     if speed_up_exp < 5:
-        # renew_latency = latency_eb3
-        # for i in range(0, 30):
-        #     new_total_latency = 0
-        #     latency_idx = np.argsort(latency_eb3)
-        #     # print('latency index:', latency_idx)
-        #     # print('rmse_sort:', np.argsort(wrmse4))
-        #     # re-sort
+        renew_latency = latency_eb3
+        for i in range(0, 30):
+            new_total_latency = 0
+            latency_idx = np.argsort(latency_eb3)
+            # print('latency index:', latency_idx)
+            # print('rmse_sort:', np.argsort(wrmse4))
+            # re-sort
   
-        #     # for latency_idx in range(0, latency_idx.shape[0]):
-        #     #     n = latency_idx*6 + 1
-        #     #     new_rank = float(mse_list[n])
-        #     #     new_rank = np.array(new_rank)
-        #     #     new_sort_k.append(new_rank)
-        #     # print('new_sort_k:', np.argsort(new_sort_k))
+            # for latency_idx in range(0, latency_idx.shape[0]):
+            #     n = latency_idx*6 + 1
+            #     new_rank = float(mse_list[n])
+            #     new_rank = np.array(new_rank)
+            #     new_sort_k.append(new_rank)
+            # print('new_sort_k:', np.argsort(new_sort_k))
 
-        #     # print("4-bit mse idx :", np.argsort(w4_mse))
-        #     quantize_layer = latency_idx[15-i]
+            # print("4-bit mse idx :", np.argsort(w4_mse))
+            quantize_layer = latency_idx[15-i]
 
-        #     # quantize_layer = np.argsort(latency_idx)[i]
-        #     print('quantize layer :', quantize_layer)
-        #     renew_latency[quantize_layer] = latency_eb2[quantize_layer]
+            # quantize_layer = np.argsort(latency_idx)[i]
+            print('quantize layer :', quantize_layer)
+            renew_latency[quantize_layer] = latency_eb2[quantize_layer]
 
-        #     print('update layer to EB2:', quantize_layer)
+            print('update layer to EB2:', quantize_layer)
 
-        #     for i in range(0, len(latency_eb3)):
-        #         new_total_latency += float(renew_latency[i])
-        #     latency_number.append(baseline_latency/new_total_latency)
-        #     hamha_quantized_layer.append(quantize_layer)
-        #     print('After eb2 weight-quantized cycles',
-        #           new_total_latency)
-        #     speed_up = baseline_latency/new_total_latency
-        #     print('After eb2 weight-quantized speed up',
-        #           speed_up)
-        #     if(speed_up > speed_up_exp):
-        #         break
-        # print('quantized layer to eb2 :', hamha_quantized_layer)
+            for i in range(0, len(latency_eb3)):
+                new_total_latency += float(renew_latency[i])
+            latency_number.append(baseline_latency/new_total_latency)
+            hamha_quantized_layer.append(quantize_layer)
+            print('After eb2 weight-quantized cycles',
+                  new_total_latency)
+            speed_up = baseline_latency/new_total_latency
+            print('After eb2 weight-quantized speed up',
+                  speed_up)
+            if(speed_up > speed_up_exp):
+                break
+        print('quantized layer to eb2 :', hamha_quantized_layer)
 
     # Speed Up : eb2 -> eb1
         renew_latency = latency_eb2
